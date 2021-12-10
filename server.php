ecb20079b4b9116cc15056e834ea7cd21e47d4d7
mojeedkusimo <mkusimo90@gmail.com>
2021-12-10 22:41:02 +0100

    Draft design for form

---
+++ server.php
@@ -0,0 +1,26 @@
+<?php
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database_name";
+
+// Create connection
+$conn = new mysqli($servername,
+	$username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: "
+		. $conn->connect_error);
+}
+
+$sqlquery = "INSERT INTO table VALUES
+	('John', 'Doe', 'john@example.com')"
+
+if ($conn->query($sql) === TRUE) {
+	echo "record inserted successfully";
+} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
